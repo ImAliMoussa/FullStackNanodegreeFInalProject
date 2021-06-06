975d957502d9a61a2e562efdf2f19f6b0202365a
Ali Moussa <imalimoussa@gmail.com>
2021-06-06 17:21:04 +0200

    added methods for updating, deleting and commiting a database object

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 import enum
+from datetime import date
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, String, Integer, Enum
+from sqlalchemy import Column, String, Integer, Enum, Date
 
 db = SQLAlchemy()
 
@@ -16,7 +17,7 @@ def setup_db(app):
     db.app = app
     db.init_app(app)
     db.create_all()
-    migrate = Migrate(app, db)
+    Migrate(app, db)
 
 
 class GenderEnum(enum.Enum):
@@ -38,3 +39,48 @@ class Actor(db.Model):
     name = Column(String)
     age = Column(Integer)
     gender = Column(Enum(GenderEnum))
+
+    class Movie(db.Model):
+        """
+        Database model for a movie
+        """
+        id = Column(Integer, primary_key=True)
+        title = Column(String)
+        release_date = Column(Date, default=date.today)
+
+        def insert(self):
+            db.session.add(self)
+            db.session.commit()
+
+        def update(self):
+            db.session.commit()
+
+        def delete(self):
+            db.session.delete(self)
+            db.session.commit()
+
+        def format(self):
+            return f'Actor: \n{vars(self)}'
+
+
+class Movie(db.Model):
+    """
+    Database model for a movie
+    """
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date, default=date.today)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return f'Movie: \n{vars(self)}'
