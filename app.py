201cebbfba3102f13e4c4470bbceeb51123db1fc
Ali Moussa <imalimoussa@gmail.com>
2021-06-06 16:04:03 +0200

    added error handlers and cors

--- app.py
+++ app.py
@@ -1,13 +1,87 @@
 import os
+from models import setup_db
+from http import HTTPStatus
+
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
-    CORS(app)
+    app.config.from_object(os.environ['APP_SETTINGS'])
+    setup_db(app)
+    CORS(app, resources={r"*": {"origins": "*"}})
+
+    """
+    Use the after_request decorator to set Access-Control-Allow
+    """
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add("Access-Control-Allow-Headers",
+                             "Content-Type,Authorization,true")
+        response.headers.add("Access-Control-Allow-Methods",
+                             "GET,PATCH,POST,DELETE,OPTIONS")
+        return response
+
+    @app.route('/')
+    def hello():
+        return 'hello world'
+
+    @app.errorhandler(HTTPStatus.BAD_REQUEST)
+    def bad_request_400(error):
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": HTTPStatus.BAD_REQUEST,
+                    "message": HTTPStatus.BAD_REQUEST.phrase,
+                }
+            ),
+            HTTPStatus.BAD_REQUEST,
+        )
+
+    @app.errorhandler(HTTPStatus.NOT_FOUND)
+    def not_found_404(error):
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": HTTPStatus.NOT_FOUND,
+                    "message": HTTPStatus.NOT_FOUND.phrase,
+                }
+            ),
+            HTTPStatus.NOT_FOUND,
+        )
+
+    @app.errorhandler(HTTPStatus.UNPROCESSABLE_ENTITY)
+    def unprocessable_entity_422(error):
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": HTTPStatus.UNPROCESSABLE_ENTITY,
+                    "message": HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+                }
+            ),
+            HTTPStatus.UNPROCESSABLE_ENTITY,
+        )
+
+    @app.errorhandler(HTTPStatus.INTERNAL_SERVER_ERROR)
+    def internal_server_error_500(error):
+        return (
+            jsonify(
+                {
+                    "success": False,
+                    "error": HTTPStatus.INTERNAL_SERVER_ERROR,
+                    "message": HTTPStatus.INTERNAL_SERVER_ERROR.phrase,
+                }
+            ),
+            HTTPStatus.INTERNAL_SERVER_ERROR,
+        )
 
     return app
 
--- models.py
+++ models.py
@@ -1,1 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+
+def setup_db(app):
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
