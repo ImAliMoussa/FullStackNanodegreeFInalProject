e2e81e3881d77ca979f7f857fe2bb1e98283e0a7
Ali Moussa <imalimoussa@gmail.com>
2021-06-10 15:22:53 +0200

    patch heroku sqlalchemy error

--- config.py
+++ config.py
@@ -3,12 +3,22 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+# heroku sqlalchemy issue
+# replace postgres:// with postgresql://
+# https://bit.ly/3pDwYrv
+def get_database_url(s: str):
+    uri = os.getenv(s, "")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    return uri
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = get_database_url('DATABASE_URL')
 
 
 class ProductionConfig(Config):
@@ -27,4 +37,4 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL_TEST', '')
+    SQLALCHEMY_DATABASE_URI = get_database_url('DATABASE_URL_TEST')
